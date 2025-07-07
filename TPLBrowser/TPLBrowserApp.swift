c095e9685edb688db55c03c06baee0f3b8c24e1b
shaheem <shanofficial2000@gmail.com>
2025-07-07 00:24:23 -0400

    Initial Commit

---
+++ TPLBrowser/TPLBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  TPLBrowserApp.swift
+//  TPLBrowser
+//
+//  Created by Shaheem on 2025-07-07.
+//
+
+import SwiftUI
+
+@main
+struct TPLBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
